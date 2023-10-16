1d1f2a6be0d236ec81d926c06bad77b321966d19
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2023-10-16 23:37:56 +0530

    PHP TUTORIALS Switch Case

---
+++ SwitchCase.php
@@ -0,0 +1,18 @@
+<?php
+    $age = 12;
+    switch($age){
+        case 12:
+            echo "You are 12 years old<br>";
+            break;
+        case 45:
+            echo "You are 45 years old<br>";
+            break;
+        case 56:
+            echo "You are 56 year old boy<br>";
+            break;
+        default:
+            echo "Your age is weird<br>";
+    }
+
+
+?>
