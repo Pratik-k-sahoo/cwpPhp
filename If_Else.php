38b79d93c9e3809cf8a56085b22dbb95f8a4eeb8
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2023-10-16 23:36:57 +0530

    PHP TUTORIALS If_Else Statement

---
+++ If_Else.php
@@ -0,0 +1,21 @@
+<?php
+
+    $a = 65;
+    $b = 9;
+
+    if($a > 78){
+        echo "a is greater than 78.<br>";
+    }else{
+        echo "a is not greater than 78.<br>";
+    }
+
+    $age = 54;
+    if($age > 18){
+        echo "You can drink water with chai and alcohol.<br>";
+    }elseif($age > 13){
+        echo "You can drink chai only with water. No alcohol for you.<br>";
+    }else{
+        echo "You can drink water only.<br>";
+    }
+
+?>
