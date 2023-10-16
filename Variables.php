a0cb03d0638f51f8778733c3ac57649fb28f032c
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2023-10-16 23:38:45 +0530

    PHP TUTORIALS Variables

---
+++ Variables.php
@@ -0,0 +1,16 @@
+<?php
+    echo "Hello World<br>";
+    //Variables are container for storing info
+    //Starts with a $
+    //Variables are case sensitive
+    $name = "Pratik";
+    $income = 2000;
+
+    echo "This guy is $name and his income is Rs. $income per month.<br>";
+    $name = "Sipun";
+    echo "$name is a good Boy<br>";
+    $name = "Pratik Kumar Sahoo";
+    echo "$name is a real gangsta.";
+
+
+?>
